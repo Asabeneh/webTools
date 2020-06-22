83dc96d705bd7655093538d77fcfccef9310800d
Asabeneh <asabeneh@gmail.com>
2020-06-22 07:39:30 +0300

    'refactored'

---
+++ generate_hexa_color.js
@@ -0,0 +1,11 @@
+const hexaColor = function() {
+    let str = '0123456789abcdef';
+    let color = '';
+    for (let i = 0; i < 6; i++) {
+    let index = Math.floor(Math.random() * str.length);
+    color += str[index];
+    }
+    return '#' + color;
+};
+
+module.exports.hexaColor = hexaColor;
---
+++ generate_rgb_color.js
@@ -0,0 +1,13 @@
+const rgbColor = function(alpha = 1) {
+    let rgb
+    let redColor = Math.floor(Math.random() * 256);
+    let greenColor = Math.floor(Math.random() * 256);
+    let blueColor = Math.floor(Math.random() * 256);
+    
+    alpha == 1 ? rgb = `rgb(${redColor},${greenColor},${blueColor})` : rgb = `rgba(${redColor},${greenColor},${blueColor},${alpha})`
+    
+    return rgb;
+};
+
+
+module.exports.rgbColor = rgbColor;
